0cd0116b4bec7d186039924c1bbb42d911525216
Olivia010101 <lavender010101@proton.me>
2025-03-11 00:26:55 +0800

    new subscription

---
+++ clash/get_nodes_85la.py
@@ -0,0 +1,44 @@
+import requests
+import yaml
+from lxml import etree
+
+# 目标网页URL
+url = "https://www.85la.com/internet-access/free-network-nodes"
+
+# 发送HTTP请求获取网页内容
+response = requests.get(url)
+html_content = response.text
+
+# 使用lxml解析HTML
+html = etree.HTML(html_content)
+
+target_xpath = '//*[@id="paging-aa"]/div[1]/div[2]/h2/a'  # 示例XPath路径
+
+# 获取目标元素的值
+# elements = print(len(elements))
+new_url = html.xpath(target_xpath)[0].attrib["href"]  # 获取属性值
+
+response = requests.get(new_url)
+html_content = response.text
+html = etree.HTML(html_content)
+target_xpath = '//*[@id="md_content_2"]/div/div[5]/div[3]/p/a'
+subscript_url = html.xpath(target_xpath)[0].text.strip()
+
+
+response = requests.get(subscript_url)
+response.raise_for_status()
+yaml_content = yaml.safe_load(response.text)
+
+proxies = yaml_content["proxies"]
+
+if proxies is not None:
+    # 创建一个新的YAML结构
+    new_yaml_content = {"proxies": proxies}
+
+    # 将新的YAML结构写入到文件
+    with open("clash/85la.yaml", "w", encoding="utf-8") as file:
+        yaml.dump(new_yaml_content, file, allow_unicode=True)
+    print("字段已成功写入到 85la.yaml 文件中")
+else:
+    print("未找到目标字段，无法写入文件")
+
